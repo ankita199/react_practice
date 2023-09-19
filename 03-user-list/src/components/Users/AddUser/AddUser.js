e9305e95705bc5ad5f8b78c45a145f7b4f49fc7c
ankita <ankita.agrawal1113@gmail.com>
2023-09-18 20:03:48 -0700

    wrapper added

--- 03-user-list/src/components/Users/AddUser/AddUser.js
+++ 03-user-list/src/components/Users/AddUser/AddUser.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Card from '../../UI/Card/Card.js'
 import Button from '../../UI/Button/Button.js'
 import ErrorModal from '../../UI/ErrorModal/ErrorModal.js'
+import Wrapper from '../../Helpers/Wrapper.js'
 import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
@@ -38,7 +39,7 @@ const AddUser = (props) => {
     setError(null)
   }
   return(
-    <div>
+    <Wrapper>
       {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
@@ -49,7 +50,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   )
 }
 
