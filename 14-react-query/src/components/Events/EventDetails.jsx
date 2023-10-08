92a819c98bc94836a8974b9ae1d1936b1544d3e3
ankita <ankita.agrawal1113@gmail.com>
2023-10-08 14:09:09 -0700

    react query edit and delte added

--- 14-react-query/src/components/Events/EventDetails.jsx
+++ 14-react-query/src/components/Events/EventDetails.jsx
@@ -1,8 +1,31 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { deleteEvent, fetchEvent, queryClient } from "../../util/http.js";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 export default function EventDetails() {
+  let { id } = useParams();
+  const navigate = useNavigate();
+
+  console.log(id);
+  const { data, isError, isPending, error } = useQuery({
+    queryKey: ["event", { id: id }],
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+  });
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["events"],
+        refetchType: "none",
+      });
+      navigate("/events");
+    },
+  });
+  function handleDelete() {
+    mutate({ id });
+  }
   return (
     <>
       <Outlet />
@@ -11,25 +34,31 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+      {isPending && <p>Loading...</p>}
+      {!isPending && data && (
+        <article id="event-details">
+          <header>
+            <h1>{data.title}</h1>
+            <nav>
+              <button onClick={handleDelete}>Delete</button>
+              <Link to="edit">Edit</Link>
+            </nav>
+          </header>
+          <div id="event-details-content">
+            <img src={`http://localhost:3000/${data.image}`} alt="" />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">{data.location}</p>
+                <time dateTime={`Todo-DateT$Todo-Time`}>
+                  {data.date}&nbsp;&nbsp;
+                  {data.time}
+                </time>
+              </div>
+              <p id="event-details-description">{data.description}</p>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
           </div>
-        </div>
-      </article>
+        </article>
+      )}
     </>
   );
 }
